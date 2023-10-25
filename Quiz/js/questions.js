2a5356e46957dbb00a56ace179e30cb14e131145
Mnelisi Maswanganyi <142810215+MnelisiMaswanganyi@users.noreply.github.com>
2023-10-25 14:03:50 +0200

    js files for the quiz

---
+++ Quiz/js/questions.js
@@ -0,0 +1,131 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+  numb: 3,
+  question: " _______ is the process of finding errors and fixing them within a program",
+  answer: "Debugging",
+  options: [
+    "Compiling",
+    "Executing",
+    "Debugging",
+    "Scanning"
+  ]
+  },
+    {
+    numb: 4,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+
+  },
+  {
+  numb: 6,
+  question: "Sal needs to execute a section of code ten times within a program. Compare the selection structures below and select which one meets the needs identified",
+  answer: "For",
+  options: [
+    "If-Else",
+    "For",
+    "While",
+    "If"
+  ]
+},
+  {
+  numb: 7,
+  question: "Which type of JavaScript language is ___ ",
+  answer: "Object-Based",
+  options: [
+    "Object-Oriented" ,
+    "Object-Based",
+    "Assembly-language",
+    "High-level"
+  ]
+
+},
+{
+numb: 8,
+question: "Which one of the following also known as Conditional Expression ",
+answer: "immediate if",
+options: [
+  "Alternative to if-else",
+  "Switch statement",
+  "If-then-else statement",
+  "immediate if"
+]
+},
+{
+numb: 9,
+question: "In JavaScript, what is a block of statement?",
+answer: "block that combines a number of statements into a single compound statement ",
+options: [
+  "Conditional block" ,
+  "block that combines a number of statements into a single compound statement",
+  "both conditional block and a single statement",
+  "block that contains a single statement"
+]
+  },
+    {
+    numb: 10,
+    question: "Which of the following type of a variable is volatile?",
+    answer: "Mutable variable",
+    options: [
+      "Mutable variable",
+      "Dynamic variable",
+      "Volatile variable",
+      "Immutable variable"
+    
+      
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
